51bf72f75ec14f277643477fc6c1d89bf22b3ef3
polygraph-umg <russell@polygraph.cool>
2024-08-16 13:36:33 -0400

    scrolly setup

--- google.config.js
+++ google.config.js
@@ -1,11 +1,6 @@
 export default [
 	{
-		"id": "1352iFuTSDDFPNAXBbOaEXkF8mCdjPu6B43hpUF2P3C4",
-		"filepath": "src/data/copy.json"
-	},
-	{
-		"id": "1te65h_nywgXVAzvV-9E5fmHGblcKvctf_cNZgeam9Tk",
-		"gid": "0",
-		"filepath": "src/data/test.csv"
+		id: "1aH7epgI6aNp7Xbh1XxX4TZLcgQlXmvjiTHMyoeC8bQI",
+		filepath: "src/data/copy.json"
 	}
-]
+];
--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,13 +1,78 @@
 <script>
 	import { getContext } from "svelte";
-	import Demo from "$components/demo/Demo.svelte";
-	import WIP from "$components/helpers/WIP.svelte";
-	// import Footer from "$components/Footer.svelte";
+	import { browser } from "$app/environment";
+	import Scrolly from "$components/helpers/Scrolly.svelte";
+	import viewport from "$stores/viewport.js";
+	const copy = getContext("copy");
 
-	// const copy = getContext("copy");
-	// const data = getContext("data");
+	let scrollyValue;
+	let figureText = "";
+
+	function updateScrolly() {
+		if (scrollyValue === undefined) return;
+		const selector = `.step:nth-of-type(${scrollyValue + 1})`;
+		const id = document.querySelector(selector).getAttribute("data-id");
+		// copy.body.section[1].findIndex(scrollyValue)
+		figureText = id;
+	}
+
+	$: if (browser) updateScrolly(scrollyValue);
 </script>
 
-<WIP />
-<Demo />
-<!-- <Footer /> -->
+{#each copy.body as { section, steps }}
+	<section id={section}>
+		{#if steps}
+			<figure>
+				<p>{figureText}</p>
+			</figure>
+			<Scrolly bind:value={scrollyValue}>
+				{#each steps as { id, text }, i}
+					{@const active = scrollyValue === i}
+					<div data-id={id} class="step" class:active>
+						<p>{text}</p>
+					</div>
+				{/each}
+			</Scrolly>
+		{:else}
+			<div class="spacer"></div>
+		{/if}
+	</section>
+{/each}
+
+<style>
+	section {
+		position: relative;
+	}
+
+	.spacer {
+		height: 200svh;
+	}
+
+	.step {
+		padding-bottom: 100svh;
+	}
+
+	.step.active p {
+		color: red;
+	}
+
+	p {
+		height: 100px;
+	}
+
+	.step:last-of-type p {
+		height: 200px;
+	}
+
+	figure {
+		position: sticky;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100svh;
+		border: 4px solid blue;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
